ff28c41c51b45cecefe63a05aa6c5e0ed774628e
Suryansh Sharma <suryansharma09@gmail.com>
2024-10-09 13:56:34 +0530

    posenet integration done

--- src/flask_app/routes.py
+++ src/flask_app/routes.py
@@ -1,6 +1,3 @@
 # Contains the Flask routes (API endpoints)
 
 
-
-app.config.from_prefixed_env()
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- src/pose_estimation/posenet_integration.py
+++ src/pose_estimation/posenet_integration.py
@@ -2,3 +2,13 @@
 import cv2
 import tensorflow as tf
 import numpy as np
+
+
+#load posenet model
+model = tf.keras.models.load_model('models/pose_estimation_model/posenet_model.h5')
+
+def extract_keypoints(image):
+    image= cv2.resize(image,(236,236))
+    image= np.expand_dims(image,axis=0)/255.0
+    keypoints= model.predict(image)
+    return keypoints
