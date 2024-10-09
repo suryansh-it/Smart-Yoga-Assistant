aaccdfc3dff4ba0530d06b27b10ceca20dda2718
Suryansh Sharma <suryansharma09@gmail.com>
2024-10-09 19:19:25 +0530

    modified posenet

--- src/pose_estimation/posenet_integration.py
+++ src/pose_estimation/posenet_integration.py
@@ -3,13 +3,24 @@
 import cv2
 import tensorflow as tf
 import numpy as np
+import tensorflow_hub as hub
 
 
-#load posenet model
-model = tf.keras.models.load_model('models/pose_estimation_model/posenet_model.h5')
 
-def extract_keypoints(image):
-    image= cv2.resize(image,(236,236))
-    image= np.expand_dims(image,axis=0)/255.0
-    keypoints= model.predict(image)
+# Load the PoseNet model from TensorFlow Hub (pretrained)
+model = hub.load("https://tfhub.dev/google/movenet/singlepose/lightning/4")
+
+def get_posenet_keypoints(image):
+    """
+    Extract keypoints using PoseNet for basic body poses (16 keypoints).
+    :param image: Input image for pose estimation.
+    :return: List of keypoints for the body (nose, shoulders, elbows, hips, knees, etc.).
+    """
+    input_image = cv2.resize(image, (192, 192))  # Resize image to match model input
+    input_image = input_image.astype(np.float32) / 255.0  # Normalize image
+    input_image = np.expand_dims(input_image, axis=0)  # Expand dimensions for batch size
+    keypoints = model(input_image)
+
+    # Extract keypoints from model output
+    keypoints = keypoints['output_0'].numpy()[0]
     return keypoints
