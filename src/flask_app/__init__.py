457cea6f869183115e8665cd8842f505bd813df9
Suryansh Sharma <suryansharma09@gmail.com>
2024-10-10 20:51:05 +0530

    form for user auth

--- src/flask_app/__init__.py
+++ src/flask_app/__init__.py
@@ -2,6 +2,11 @@
 from flask import Flask
 from flask_migrate import Migrate
 from .models import db
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
@@ -11,9 +16,14 @@ def create_app():
 
     db.init_app(app)
 
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'  # Redirect to login if not authenticated
 
     from .routes import main
+    from .auth import auth
     app.register_blueprint(main)
+    app.register_blueprint(auth)
 
     migrate = Migrate(app,db)
     with app.app_context():
--- src/flask_app/models.py
+++ src/flask_app/models.py
@@ -1,12 +1,16 @@
 # Database models for PostgreSQL (User, Pose, Feedback)
 
+
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model , UserMixin):
     __tablename__ = 'User_Table'
     id = db.Column(db.Integer, primary_key = True)
     username  = db.Column(db.String(80) , unique = True, nullable = False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
 
 class Pose(db.Model):
     __tablename__ = 'Pose_Table'
@@ -21,3 +25,7 @@ class Feedback(db.Model):
     user_id= db.Column(db.Integer,db.ForeignKey('User_Table.id'))
     pose_id= db.Column(db.Integer,db.ForeignKey('Pose_Table.id'))
     feedback_text = db.Column(db.String(255))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
