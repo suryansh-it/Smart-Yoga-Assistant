d2531b745bdfdf0b56cd68fd14112d593a48629a
Suryansh Sharma <suryansharma09@gmail.com>
2024-10-10 18:25:03 +0530

    form model done

--- src/classifiers/pose_classifier.py
+++ src/classifiers/pose_classifier.py
@@ -1,1 +1,7 @@
 # Code for loading and using Yoga pose classification model
+
+import tensorflow as tf
+
+class PoseClassifier:
+    def __init__(self, model_path):
+        self.model = tf.keras.models.load_model(model_path)
--- src/flask_app/forms.py
+++ src/flask_app/forms.py
@@ -1,1 +1,11 @@
 # Flask-WTF Forms for user interaction
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+class FeedbackForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    content = TextAreaField('Feedback', validators=[DataRequired()])
+    submit = SubmitField('Submit Feedback')
