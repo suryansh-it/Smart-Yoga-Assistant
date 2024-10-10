e3ba593fae1d8c249be2fedaaa9e08b96acf8170
Suryansh Sharma <suryansharma09@gmail.com>
2024-10-11 00:49:42 +0530

    added basic templates

--- src/flask_app/routes.py
+++ src/flask_app/routes.py
@@ -2,6 +2,7 @@
 
 # routes.py
 from flask import Blueprint, render_template, request
+from flask_login import login_required
 from src.live_feedback.video_capture import capture_video
 
 main = Blueprint('main', __name__)
@@ -11,10 +12,12 @@ def index():
     return render_template('index.html')
 
 @main.route('/feedback')
+@login_required
 def feedback():
     return render_template('feedback.html')
 
 @main.route('/start', methods=['POST'])
+
 def start_feedback():
     capture_video(use_posenet=True)
     return "Yoga feedback session ended"
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from .flask_app import create_app
+from flask_app import create_app
 
 app = create_app()
 
