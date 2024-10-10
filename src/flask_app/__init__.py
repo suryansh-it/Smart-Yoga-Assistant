93302bab8933acc2c4d2ca8215d6dff8b6053e50
Suryansh Sharma <suryansharma09@gmail.com>
2024-10-10 18:58:06 +0530

    modified init

--- src/flask_app/__init__.py
+++ src/flask_app/__init__.py
@@ -10,6 +10,11 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+
+
+    from .routes import main
+    app.register_blueprint(main)
+
     migrate = Migrate(app,db)
     with app.app_context():
         db.create_all()
--- src/live_feedback/real_time_pose_eval.py
+++ src/live_feedback/real_time_pose_eval.py
@@ -1,1 +1,12 @@
 # Code for real-time evaluation and feedback
+
+# from src.pose_estimation.posenet_integration import get_posenet_keypoints
+# from src.pose_estimation.mediapipe_integration import get_mediapipe_keypoints
+# from src.classifiers.pose_classifier import YogaPoseClassifier
+# import cv2
+
+# classifier = YogaPoseClassifier('path/to/pretrained/model.h5')
+
+# def evaluate_pose(image, use_posenet=True):
+#     if use_posenet:
+#         keypoints = get_posenet_keypoints(image)
